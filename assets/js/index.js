44b5e2d0259fb056d82fbeedd9687600642eda22
Stefan Matei <smatei@gmail.com>
2017-02-08 17:07:26 +0200

    added bootstrap.js. concatenate third-party js files into app.js. use webpack on js files so that we can use "import".

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+import * as Vue from './vue';
+
+new Vue({
+    el: '#minion-table'
+});
--- gulpfile.js
+++ gulpfile.js
@@ -16,11 +16,21 @@ const gulp          = require('gulp'),
       replace       = require('gulp-replace');
 
 let   src           = './assets',
-      dist          = './target/classes/static';
-
+      dist          = './target/classes/static',
+      vendorFiles   = [src + '/js/third-party/tether.js',
+                      src + '/js/third-party/bootstrap.js',
+                      src + '/js/third-party/selectize.js',
+                      src + '/js/app.js'];
 //You will need this in webpack-config, too.
 var isProduction = argv.production;
 global.IsProduction = isProduction;
+
+// Clean the dist folder
+gulp.task('clean', function() {
+  return del(dist).then(paths => {
+    console.log('Deleted '+ dist +' files and folders:\n', paths.join('\n'));
+  });
+});
 
 // Compile the SCSS files, run autoprefixer and compress the resulted CSS's
 gulp.task('css', function() {
@@ -47,4 +57,49 @@ gulp.task('css', function() {
     .pipe(gulp.dest(dist + '/css/'))
 });
 
-gulp.task('default',   ['css']);
+/*
+ Concatenate vendors JS's and app.js.
+ Use specific order to avoid missing dependencies.
+ Dependencies tasks are called synchronous.
+*/
+ gulp.task('js-concatenate', function() {
+   gulp.start('js', function() {
+     gulp.start('webpack', function(){});
+   });
+
+  return gulp.src(vendorFiles)
+            .pipe(concat('app.js'))
+            .pipe(uglify({preserveComments: 'license'}))
+            .pipe(gulp.dest(dist + '/js/'));
+});
+
+// Uglify the JS's
+gulp.task('js', function () {
+  var toReturn = gulp.src([      src + '/js/**/*.js',
+                   '!' + src + '/js/third-party/**/*.js']);
+  return JSTask(toReturn, dist + '/js/');
+});
+
+function JSTask(aGulpSRC, aDestinationFolder) {
+  return  aGulpSRC.pipe(babel({
+            presets: ['es2015']
+          }))
+          .pipe(uglify({preserveComments: 'license'}).on('error', function (e) {
+              console.log(e);
+          }))
+          .pipe(gulp.dest(aDestinationFolder));
+}
+
+/*
+  Makes modules-loading available using webpack library:
+   https://webpack.github.io/
+*/
+gulp.task('webpack', function () {
+  return gulp.src([      dist + '/js/**/*.js'])
+    .pipe(webpack(require('./webpack-config.js'), webpack2))
+    .pipe(gulp.dest(dist + '/js/'));
+});
+
+gulp.task('default',   ['css', 'js-concatenate']);
+
+//gulp.task('default',   ['clean']);
